7031a479e09281b2b3e749786b900c04f74523b1
Roberto Herrera <robeherrera561.2@gmail.com>
2022-01-28 21:37:08 -0600

    Ejemplo de Corrutinas con Launch

--- app/src/main/java/com/robe/corrutinas/Corrutinas.kt
+++ app/src/main/java/com/robe/corrutinas/Corrutinas.kt
@@ -1,5 +1,111 @@
 package com.robe.corrutinas
 
-fun main(){
+import kotlinx.coroutines.*
+import java.lang.Thread.sleep
+import java.util.*
 
+fun main() {
+    /* Ejemplo de Bloqueo */
+//    println("Tarea 1 " + Thread.currentThread().name + " -- " + Date().seconds)
+//    tareaPesada("Tarea 2")
+//    println("Tarea 3 " + Thread.currentThread().name + " -- " + Date().seconds)
+
+    /* Ejemplo de Suspend */
+    //otraFuncion()
+
+    /* Ejemplo de dispatcher */
+    //dispatchersEjemplos()
+
+    /* Launch */
+    funcionLaunch()
+}
+
+fun funcionLaunch() {
+    println("Tarea 1 " + Thread.currentThread().name + " -- " + Date().seconds)
+    GlobalScope.launch {
+        corrutina("☻")
+    }
+    GlobalScope.launch(newSingleThreadContext("Mi Hilito")) {
+        corrutina("♥")
+    }
+    println("Tarea 3 " + Thread.currentThread().name + " -- " + Date().seconds)
+    for (number in 1..10) {
+        sleep(100)
+        print(" m" + number)
+    }
+}
+
+fun corrutina(s: String) {
+    for (number in 1..9) {
+        sleep(100)
+        print(" "+s+number)
+    }
+    println("\n"+s+ " -- " + Thread.currentThread().name)
+}
+
+fun dispatchersEjemplos() {
+    // sin dispatcher -> hilo donde se encuentra
+    runBlocking {
+        //println("Hilo 1 : 0 + ${Thread.currentThread().name}")
+        tareaPesada3("Pesada con SUSPEND -- ")
+    }
+
+    tareaPesada2("Pesada con runBlocking -- ")
+
+    // Le dices a Kotlin haz lo que quieras (Cualquiera)
+    runBlocking(Dispatchers.Unconfined) {
+        println("Hilo 1 : 1 + ${Thread.currentThread().name}")
+    }
+
+    // Donde Estoy , basicamente que es igual al primero (sin parametros)
+    runBlocking(Dispatchers.Default) {
+        println("Hilo 1 : 2 + ${Thread.currentThread().name}")
+    }
+
+    //Wen Services y bases de datos
+    runBlocking(Dispatchers.IO) {
+        println("Hilo 1 : 3 + ${Thread.currentThread().name}")
+    }
+
+    // No abusar y parece ser que esta obsoleto
+    //@ObsoleteCoroutinesApi
+    runBlocking(newSingleThreadContext("Mi hilo")) {
+        println("Hilo 1 : 4 + ${Thread.currentThread().name}")
+    }
+
+//    runBlocking(Dispatchers.Main) {
+//        println("Hilo 1 : 0 + ${Thread.currentThread().name}")
+//    }
+
+}
+
+
+fun otraFuncion() {
+    println("Tarea 1 " + Thread.currentThread().name + " -- " + Date().seconds)
+
+    // Otra forma de hacer el RunBlocking
+    /*runBlocking{
+        tareaPesada2("Tarea 2")
+    }*/
+
+    tareaPesada2("Tarea 2")
+    println("Tarea 3 " + Thread.currentThread().name + " -- " + Date().seconds)
+}
+
+fun tareaPesada(msg: String) {
+    println("cargando... ${msg} " + Thread.currentThread().name + " -- " + Date().seconds)
+    Thread.sleep(5000)
+    println("dejo de cargar.... ${msg} " + Thread.currentThread().name + " -- " + Date().seconds)
+}
+
+fun tareaPesada2(msg: String) = runBlocking {
+    println("cargando... ${msg} " + Thread.currentThread().name + " -- " + Date().seconds)
+    Thread.sleep(5000)
+    println("dejo de cargar.... ${msg} " + Thread.currentThread().name + " -- " + Date().seconds)
+}
+
+suspend fun tareaPesada3(msg: String) {
+    println("cargando... ${msg} " + Thread.currentThread().name + " -- " + Date().seconds)
+    sleep(5000)
+    println("dejo de cargar.... ${msg} " + Thread.currentThread().name + " -- " + Date().seconds)
 }
--- app/src/main/java/com/robe/corrutinas/MainActivity.kt
+++ app/src/main/java/com/robe/corrutinas/MainActivity.kt
@@ -2,10 +2,42 @@ package com.robe.corrutinas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+    lateinit var button: Button
+    lateinit var button2: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        incializar()
+        button.setOnClickListener{
+            GlobalScope.launch {
+                corrutina()
+            }
+        }
+        button2.setOnClickListener {
+            mensaje(R.string.mensaje2)
+        }
+
+    }
+
+    suspend fun corrutina() {
+        delay(2_000)
+        mensaje(R.string.mensaje)
+    }
+
+    private fun incializar() {
+        button = findViewById(R.id.button1)
+        button2 = findViewById(R.id.button2)
+    }
+
+    private fun mensaje(mensaje: Int) {
+//            Toast.makeText(applicationContext, R.string.mensaje2,Toast.LENGTH_LONG).show()
+        println(getString(mensaje))
     }
 }
